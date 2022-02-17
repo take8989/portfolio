406155d305b75c1be9d2056531e5900563ab30cb
ashgenki <houkingu6416@gmail.com>
2022-02-17 11:54:43 +0900

    main

--- common.js
+++ common.js
@@ -1,12 +1,17 @@
+//トップに戻るボタン
 $(function () {
   $("#topbtn").hide();
   $(window).scroll(function () {
+
+//トップから801px下にスクロールした時にボタンが現れる
       if ($(window).scrollTop() > 800) {
           $("#topbtn").fadeIn(1000);
       } else {
           $("#topbtn").fadeOut();
       }
   });
+  
+//ボタンを押したときトップに戻る
   $("#topbtn").click(function () {
       $("body,html").animate({scrollTop: 0}, 500);
       return false;
